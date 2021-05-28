60fb60843f9300ec3fb75cfaf78eb4b38e267330
Ryan Su <suxi@oregonstate.edu>
2021-05-28 16:54:34 -0700

    fix linter

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import unittest
 import example
 
+
 class TestCase(unittest.TestCase):
+
+    
     def test_one(self):
-        self.assertEqual(example.add(5,5), 10)
+        self.assertEqual(example.add(5, 5), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
