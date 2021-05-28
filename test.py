9f8111b7c90e7dd4bda4c4a59a1bc559b4c0e9c4
Ryan Su <suxi@oregonstate.edu>
2021-05-28 16:59:56 -0700

    create more tests

--- test.py
+++ test.py
@@ -7,6 +7,15 @@ class TestCase(unittest.TestCase):
     def test_one(self):
         self.assertEqual(example.add(5, 5), 10)
 
+    def test_one(self):
+        self.assertEqual(example.subtract(15, 5), 5)
+
+    def test_one(self):
+        self.assertEqual(example.multiply(5, 5), 25)
+
+    def test_one(self):
+        self.assertEqual(example.divide(25, 5), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
